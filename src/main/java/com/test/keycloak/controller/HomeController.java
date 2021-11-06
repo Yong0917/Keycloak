d9267a8c22c6ea1149c643b78661b920950d014f
Yong0917 <ssyong917@naver.com>
2021-11-06 13:37:50 +0900

    Controller model 추가 및 session 설정

--- src/main/java/com/test/keycloak/controller/HomeController.java
+++ src/main/java/com/test/keycloak/controller/HomeController.java
@@ -1,27 +1,56 @@
 package com.test.keycloak.controller;
 
+import com.test.keycloak.api.KeycloakApi;
+import com.test.keycloak.vo.UserVO;
+import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
 
+    @Autowired
+    private KeycloakApi keycloakApi;
+
+
     @GetMapping("/")
-    public String Home(){
+    public String Home(HttpSession session){
+
+        String id = (String) session.getAttribute("id");
+
+        if(id==null || "".equals(id)){
+            session.setAttribute("id","");
+        }
+
         return "main";
     }
 
     @GetMapping("/main")
-    public String Main(){
+    public String Main(Model model, HttpSession session, KeycloakAuthenticationToken authentication){
+
+        UserVO result = userSet(authentication, session);
+
+        model.addAttribute("userInfo",result);
+
         return "main";
     }
 
 
     @GetMapping("/user_manage")
-    public String userManage(){
+    public String userManage(Model model, KeycloakAuthenticationToken authentication,HttpSession session){
+
+        UserVO result = userSet(authentication, session);
+
+        model.addAttribute("userInfo",result);
+
         return "user_manage";
     }
 
@@ -31,18 +60,54 @@ public class HomeController {
     }*/
 
     @GetMapping("/my_info")
-    public String myInfo(){
+    public String myInfo(Model model, KeycloakAuthenticationToken authentication,HttpSession session){
+
+        UserVO result = userSet(authentication, session);
+
+        model.addAttribute("userInfo",result);
+
         return "my_info";
+
     }
 
     @GetMapping("/pw_manage")
-    public String pwManage(){
+    public String pwManage(Model model, KeycloakAuthenticationToken authentication, HttpSession session){
+
+        UserVO result = userSet(authentication, session);
+
+        model.addAttribute("userInfo",result);
+
         return "pw_manage";
+
     }
 
-    @GetMapping("/err/denied-page")
-    public String errPage(){
-        return "denied-page";
+
+    private UserVO userSet(KeycloakAuthenticationToken authentication, HttpSession session){
+
+        SimpleKeycloakAccount account = (SimpleKeycloakAccount) authentication.getDetails();
+        AccessToken token = account.getKeycloakSecurityContext().getToken();
+
+        String uuid =  authentication.getPrincipal().toString();        //로그인 id 값
+        String userName = token.getPreferredUsername();         //로그인 id
+
+        UserVO result = keycloakApi.userInfoList(uuid, userName);
+
+        session.setAttribute("id", result.getId());
+
+        return result;
     }
+
+//    SimpleKeycloakAccount account = (SimpleKeycloakAccount) authentication.getDetails();
+//    AccessToken token = account.getKeycloakSecurityContext().getToken();
+//
+//    UserVO userVO = new UserVO();
+//        userVO.setId(token.getPreferredUsername());
+//        userVO.setFirstName(token.getFamilyName());
+//        userVO.setLastName(token.getGivenName());
+//        userVO.setEmail(token.getEmail());
+//
+//    String userName = token.getPreferredUsername();
+//
+//        session.setAttribute("id", userName);
 
 }
